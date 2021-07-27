780fb489d8df81cfa9b421a845160eb8cd8e13b1
Seoyeon Kim <seoyeonkim815@gmail.com>
2021-07-27 14:51:48 +0900

    [Update] Added global styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { BrowserRouter as Router } from 'react-router-dom'
+import { GlobalStyles } from './GlobalStyles.js'
+import Header from './components/Header'
+import Section1 from './components/Section1'
+import Footer from './components/Footer'
+
 function App() {
   return (
-    <div className="App">
-      Hello world!
-    </div>
+    <Router>
+      <GlobalStyles />
+      <div className="App">
+        <Header />
+        <Section1 />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
